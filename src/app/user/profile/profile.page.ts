e3b8ae8100b66b103092f06720ff0676a6c8eb0e
Your Name <you@example.com>
2021-04-19 11:45:53 -0300

    perfilusuario

--- src/app/user/profile/profile.page.ts
+++ src/app/user/profile/profile.page.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 // 1) Importa
 import { AngularFireAuth } from '@angular/fire/auth';
 
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
@@ -10,11 +13,26 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class ProfilePage implements OnInit {
 
+  // Atributos
+  private itemDoc: AngularFirestoreDocument<any>;
+  item: Observable<any>;
+
   constructor(
 
     // 2) Injeta
-    public auth: AngularFireAuth
-  ) { }
+    public auth: AngularFireAuth,
+    public afs: AngularFirestore
+  ) { 
+
+    this.auth.onAuthStateChanged(
+      (userData) => {
+        if (userData) {
+          this.itemDoc = afs.doc<any>(`register/${userData.uid}`);
+          this.item = this.itemDoc.valueChanges();
+        }
+      }
+    );
+  }
 
   ngOnInit() { }
 
